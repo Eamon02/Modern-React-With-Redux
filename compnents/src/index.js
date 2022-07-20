9ed3496afe1bd133c4868d2b59f394b760cb5ad4
Eamon McHugh <eamonmchugh2@gmail.com>
2022-07-20 16:16:29 -0400

    finished section 3: props

--- compnents/src/index.js
+++ compnents/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
 import ApprovalCard from './ApprovalCard';
+import './style/App.css';
 
 const App = () => {
 	return (
@@ -15,18 +16,22 @@ const App = () => {
 					commentText='Awesome stuff!!!'
 				/>
 			</ApprovalCard>
-			<CommentDetail
-				icon={faker.image.image()}
-				author='Alex'
-				postTime='6:00'
-				commentText='Cool story!'
-			/>
-			<CommentDetail
-				icon={faker.image.image()}
-				author='Beans'
-				postTime='7:00'
-				commentText='nice post'
-			/>
+			<ApprovalCard>
+				<CommentDetail
+					icon={faker.image.image()}
+					author='Alex'
+					postTime='6:00'
+					commentText='Cool story!'
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					icon={faker.image.image()}
+					author='Beans'
+					postTime='7:00'
+					commentText='nice post'
+				/>
+			</ApprovalCard>
 		</>
 	);
 };
